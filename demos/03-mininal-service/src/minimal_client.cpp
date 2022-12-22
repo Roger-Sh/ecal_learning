ece5f6eb827a03f96e502cc43c44c3039278fcb9
Weipu Shan <weipu.shan@foxmail.com>
2022-12-22 14:16:57 +0800

    demo03 minimal service cleanup

---
+++ demos/03-mininal-service/src/minimal_client.cpp
@@ -0,0 +1,109 @@
+#include <ecal/ecal.h>
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+// callback for service response
+void OnServiceResponse(const struct eCAL::SServiceResponse &service_response_)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+
+    switch (service_response_.call_state)
+    {
+        // service successful executed
+        case eCallState::call_state_executed:
+            std::cout << "Received response for method in CB " 
+                << service_response_.method_name << " : " 
+                << service_response_.response 
+                << " from host " << service_response_.host_name << std::endl;
+            break;
+
+        // service execution failed
+        case eCallState::call_state_failed:
+            std::cout << "Received error for method in CB " 
+                << service_response_.method_name << " : " 
+                << service_response_.error_msg 
+                << " from host " << service_response_.host_name << std::endl;
+            break;
+
+        default:
+            break;
+    }
+}
+
+// main entry
+int main(int argc, char **argv)
+{
+    // initialize eCAL API
+    eCAL::Initialize(argc, argv, "minimal client");
+
+    // create minimal service client
+    eCAL::CServiceClient minimal_client("minimal_service");
+    minimal_client.AddResponseCallback(OnServiceResponse);
+
+    while (eCAL::Ok())
+    {
+        // eCAL::SServiceResponse service_info;
+        std::string method_name("echo");
+        std::string request("Hello");
+
+        //////////////////////////////////////
+        // Service call (blocking)
+        //////////////////////////////////////
+        eCAL::ServiceResponseVecT service_response_vec;
+        if (minimal_client.Call(method_name, request, -1, &service_response_vec))
+        {
+            for (auto service_response : service_response_vec)
+            {
+                std::cout << "Method 'echo' called with message in blocking : " << request << std::endl;
+
+                switch (service_response.call_state)
+                {
+                    // service successful executed
+                    case eCallState::call_state_executed:
+                        std::cout << "Received response in blocking: " 
+                            << service_response.response 
+                            << " from host " << service_response.host_name << std::endl;
+                        break;
+                    // service execution failed
+                    
+                    case eCallState::call_state_failed:
+                        std::cout << "Received error in blocking: " 
+                            << service_response.error_msg 
+                            << " from host " << service_response.host_name << std::endl;
+                        break;
+                    
+                    default:
+                        std::cout << "service_info.call_state in blocking: " << service_response.call_state << "\n";
+                        break;
+                }
+            }
+        }
+        else
+        {
+            std::cout << "Method blocking call failed .." << std::endl
+                << std::endl;
+        }
+
+        //////////////////////////////////////
+        // Service call (with callback)
+        //////////////////////////////////////
+        if (minimal_client.Call(method_name, request))
+        {
+            std::cout << "Method 'echo' called with message in CB: " << request << std::endl;
+        }
+        else
+        {
+            std::cout << "Method callback call failed in CB .." << std::endl;
+        }
+
+        // sleep a second
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    // finalize eCAL API
+    eCAL::Finalize();
+
+    return (0);
+}
---
+++ demos/03-mininal-service/src/minimal_server.cpp
@@ -0,0 +1,45 @@
+#include <ecal/ecal.h>
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+// method callback
+int OnMethodCallback(
+    const std::string &method_, 
+    const std::string & /*req_type_*/, 
+    const std::string & /*resp_type_*/, 
+    const std::string &request_, 
+    std::string &response_)
+{
+    std::cout << "Method called : " << method_ << std::endl;
+    std::cout << "Request       : " << request_ << std::endl
+              << std::endl;
+    response_ = request_;
+    return 42;
+}
+
+// main entry
+int main(int argc, char **argv)
+{
+    // initialize eCAL API
+    eCAL::Initialize(argc, argv, "minimal server");
+
+    // create minimal service server
+    eCAL::CServiceServer minimal_server("minimal_service");
+
+    // add method callback
+    minimal_server.AddMethodCallback("echo", "", "", OnMethodCallback);
+
+    // idle
+    while (eCAL::Ok())
+    {
+        // sleep 100 ms
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    // finalize eCAL API
+    eCAL::Finalize();
+
+    return (0);
+}
